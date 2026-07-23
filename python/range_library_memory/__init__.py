6b7548af9f482d734fbbb52ff2599514d82b621f
joshuamoodley06-ux <joshuamoodley06@gmail.com>
2026-07-23 17:53:32 +0200

    feat: activate inherited Daily doctrine after approved pipeline

--- python/range_library_memory/__init__.py
+++ python/range_library_memory/__init__.py
@@ -9,11 +9,13 @@ from .schema import REQUIRED_TABLES, init_schema
 # ordinary doctrine packages after five-sample approval.
 from . import doctrine_pipeline as _doctrine_pipeline
 from . import weekly_chronology_bos as _weekly_chronology_bos
+from .daily_inherited_doctrine import install as _install_daily_inherited_doctrine
 from .doctrine_package_runtime_registry import install as _install_doctrine_packages
 from .weekly_chronology_bos_v2_registry import install as _install_weekly_v2
 
 _install_weekly_v2(_weekly_chronology_bos, _doctrine_pipeline)
 _install_doctrine_packages(_doctrine_pipeline)
+_install_daily_inherited_doctrine(_doctrine_pipeline)
 
 __all__ = [
     "DEFAULT_DB_PATH",
